b85239b05c700b3df36cfef28b77c7ec6fa8c328
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-08-08 14:11:07 +0200

    Header & Footer

--- Front/common-footer.php
+++ Front/common-footer.php
@@ -1,1 +1,67 @@
-<footer></footer>
+<footer>
+	<div class="wrapper">
+		<div class="row">
+			<div class="col">
+				<div class="logo">
+					<img src="img/common/logo.svg">
+				</div>
+				<p>Révélez le meilleur de vous même</p>
+				<div class="container-el">
+					<a href="" class="el">
+						<img src="img/footer/apple.svg">
+					</a>
+					<a href="" class="el">
+						<img src="img/footer/playstore.svg">
+					</a>
+				</div>
+			</div>
+			<div class="col">
+				<h6>Fitnext</h6>
+				<a href="">Le concept</a>
+				<a href="">Erwann Menthéour</a>
+				<a href="">Parte de poids</a>
+				<a href="">Mincir au masculin</a>
+				<a href="">Régime sans gluten</a>
+				<a href="">Mincir à la ménopause</a>
+				<a href="">Les livres</a>
+			</div>
+			<div class="container-col">
+				<div class="col">
+					<h6>Espace press</h6>
+					<a href="">Le journal des femmes</a>
+					<a href="">Le livre d'Erwann</a>
+				</div>
+				<div class="col">
+					<h6>Mentions légales</h6>
+					<a href="">Mentions légales</a>
+					<a href="">Vie privée et cookies</a>
+					<a href="">CGA</a>
+				</div>
+			</div>
+			<div class="container-col">
+				<div class="col">
+					<h6>Assistance</h6>
+					<a href="">Fitnews</a>
+					<a href="">Nous contacter</a>
+				</div>
+				<div class="col">
+					<h6>Suivez nous</h6>
+					<div class="container-sn">
+						<a href="" class="sn">
+							<img src="img/footer/fb.svg">
+						</a>
+						<a href="" class="sn">
+							<img src="img/footer/pt.svg">
+						</a>
+						<a href="" class="sn">
+							<img src="img/footer/in.svg">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="foot">
+			<p>Copyright © 2019 Fitnext - Tous droits réservés</p>
+		</div>
+	</div>
+</footer>
--- Front/common-header.php
+++ Front/common-header.php
@@ -1,3 +1,28 @@
-<header>
-	<img src="img/home/dev/header.jpg">
+<header id="header-desktop">
+	<div class="wrapper">
+		<div class="container-link">
+			<a href="">La méthode</a>
+			<a href="">Erwann Mentheour</a>
+			<a href="">Formules</a>
+			<a href="">Conseils</a>
+		</div>
+		<div class="logo">
+			<img src="img/common/logo.svg">
+		</div>
+		<div class="container-action">
+			<a href="" class="btn">
+				<span class="btn-text">Connexion</span>
+			</a>
+			<a href="" class="btn">
+				<span class="btn-text">Créer mon programme</span>
+				<span class="btn-arrow">
+					<svg viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+					  <g stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+					    <path d="M7.7 1.297l5 5-5 5M12.7 6.3h-11"/>
+					  </g>
+					</svg>
+				</span>
+			</a>
+		</div>
+	</div>
 </header>
