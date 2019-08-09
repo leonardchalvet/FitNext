947f22a369e91de2c735c4a6b3a5a4a05dbfe9e2
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-08-09 16:02:18 +0200

    Header - start responsive

--- Front/common-header.php
+++ Front/common-header.php
@@ -6,15 +6,16 @@
 			<a href="">Formules</a>
 			<a href="">Conseils</a>
 		</div>
-		<div class="logo">
+		<a href="" class="logo">
 			<img src="img/common/logo.svg">
-		</div>
+		</a>
 		<div class="container-action">
 			<a href="" class="btn">
 				<span class="btn-text">Connexion</span>
 			</a>
 			<a href="" class="btn">
 				<span class="btn-text">Créer mon programme</span>
+				<span class="btn-text">Commencer maintenant</span>
 				<span class="btn-arrow">
 					<svg viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
 					  <g stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
@@ -25,4 +26,38 @@
 			</a>
 		</div>
 	</div>
+</header>
+
+<header id="header-mobile">
+	
+	<div class="head">
+		<div class="wrapper">
+			<a class="logo" href="" >
+				<img src="img/common/logo.svg" alt="">
+			</a>
+			<div class="container-action">
+				<a class="btn" href="">
+					<span class="btn-text">Commencer</span>
+					<span class="btn-arrow">
+						<svg viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+						  <g stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+						    <path d="M7.7 1.297l5 5-5 5M12.7 6.3h-11"/>
+						  </g>
+						</svg>
+					</span>
+				</a>
+				<div class="container-burger">
+					<div class="line"></div>
+					<div class="line"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-link">
+		<div class="wrapper">
+			
+		</div>
+	</div>
+
 </header>
