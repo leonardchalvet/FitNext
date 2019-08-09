7e78efaa3910311ca69911f433dcc9badbcaf8ad
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-08-09 16:03:25 +0200

    All html UP

--- Front/index.php
+++ Front/index.php
@@ -27,37 +27,36 @@
 						<div class="bg" style="background-image: url(img/home/cover-illu.png);"></div>
 						<iframe class="video" src="https://player.vimeo.com/video/352471925?api=1&background=1&mute=0&autoplay=0&loop=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
-					<div class="container-text">
-						<h1 class="elAnim__fade anim__delayMedium_0 wrapLine">Révélez le meilleur de vous-même</h1>
-						<p class="elAnim__slide anim__delayMedium_7">
-							Le coach digital qui place votre bien être au coeur de votre transformation physique.
-						</p>
-						<div class="elAnim__slide anim__delayMedium_10">
-							<a class="btn" href="">
-								<span class="btn-text">Créer mon programme</span>
-								<span class="btn-arrow">
-									<svg viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
-									  <g stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-									    <path d="M7.7 1.297l5 5-5 5M12.7 6.3h-11"/>
-									  </g>
-									</svg>
-								</span>
-							</a>
-						</div>
-						<div class="sep elAnim__slide anim__delayMedium_12"></div>
-						<div class="container-logo">
-							<?php $delay = 14;
-								for($i=0;$i<3;$i++) { ?>
-								<div class="logo elAnim__slide anim__delayMedium_<?php echo($delay); ?>">
-									<img src="img/home/logos/france2.png">
-									<img src="img/home/logos/franceinfo.png">
-									<img src="img/home/logos/journaldimanche.png">
-									<img src="img/home/logos/elle.png">
-									<img src="img/home/logos/bfm.png">
-									<img src="img/home/logos/menhealth.png">
-								</div>
-							<?php $delay++; } ?>
-						</div>
+
+					<h1 class="elAnim__fade anim__delayMedium_0 wrapLine">Révélez le meilleur de vous-même</h1>
+					<p class="elAnim__slide anim__delayMedium_7">
+						Le coach digital qui place votre bien être au coeur de votre transformation physique.
+					</p>
+					<div class="container-btn elAnim__slide anim__delayMedium_10">
+						<a class="btn" href="">
+							<span class="btn-text">Commencer maintenant</span>
+							<span class="btn-arrow">
+								<svg viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+								  <g stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+								    <path d="M7.7 1.297l5 5-5 5M12.7 6.3h-11"/>
+								  </g>
+								</svg>
+							</span>
+						</a>
+					</div>
+					<div class="sep elAnim__slide anim__delayMedium_12"></div>
+					<div class="container-logo">
+						<?php $delay = 14;
+							for($i=0;$i<3;$i++) { ?>
+							<div class="logo elAnim__slide anim__delayMedium_<?php echo($delay); ?>">
+								<img src="img/home/logos/france2.png">
+								<img src="img/home/logos/franceinfo.png">
+								<img src="img/home/logos/journaldimanche.png">
+								<img src="img/home/logos/elle.png">
+								<img src="img/home/logos/bfm.png">
+								<img src="img/home/logos/menhealth.png">
+							</div>
+						<?php $delay++; } ?>
 					</div>
 				</div>
 			</section>
@@ -262,59 +261,71 @@
 
 			<section id="section-photo" class="sectionAnim_container">
 				<div class="el">
-					<div class="bg elAnim__fade anim__delayMedium_4" style="background-image: url(img/home/photo-bg-1.jpg);"></div>
+					<div class="bg elAnim__fade anim__delayMedium_2" style="background-image: url(img/home/photo-bg-1.jpg);"></div>
+					<div class="overlay elAnim__fade anim__delayMedium_2"></div>
 					<div class="wrapper">
 						<div class="elAnim__slide anim__delayMedium_6">
 							<svg class="icn" viewBox="0 0 42 30" xmlns="http://www.w3.org/2000/svg">
 								<path d="M42 0v30H24V12.614L42 0zM18 0v30H0V12.614L18 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 							</svg>
 						</div>
-						<h2 class="elAnim__slide anim__delayMedium_10">
+						<a href="https://www.ncbi.nlm.nih.gov/pubmed/28360349" class="elAnim__slide anim__delayMedium_10">
 							Un meilleur microbiote réduit les risques de cancers et de maladies inflammatoires et diminue les facteurs de risques cardiovasculaires. 
-						</h2>
-						<p class="elAnim__slide anim__delayMedium_12">Pathogenesis to Therapy.</p>
+						</a>
+						<div class="logo elAnim__slide anim__delayMedium_12">
+							<img src="img/home/photo-icn-1.png">
+						</div>
 					</div>
 				</div>
 				<div class="el">
 					<div class="bg" style="background-image: url(img/home/photo-bg-2.jpg);"></div>
+					<div class="overlay"></div>
 					<div class="wrapper">
 						<div class="elAnim__slide">
 							<svg class="icn" viewBox="0 0 42 30" xmlns="http://www.w3.org/2000/svg">
 								<path d="M42 0v30H24V12.614L42 0zM18 0v30H0V12.614L18 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 							</svg>
 						</div>
-						<h2>
+						<a href="https://www.revmed.ch/RMS/2011/RMS-317/Microbiote-intestinal-obesite-et-resistance-a-l-insuline">
 							 Il devient indéniable que la flore intestinale joue un rôle dans la survenue de l’obésité. 
-						</h2>
-						<p>Revue Médicale Suisse</p>
+						</a>
+						<div class="logo">
+							<img src="img/home/photo-icn-2.png">
+						</div>
 					</div>
 				</div>
 				<div class="el">
 					<div class="bg" style="background-image: url(img/home/photo-bg-3.jpg);"></div>
+					<div class="overlay"></div>
 					<div class="wrapper">
 						<div class="elAnim__slide">
 							<svg class="icn" viewBox="0 0 42 30" xmlns="http://www.w3.org/2000/svg">
 								<path d="M42 0v30H24V12.614L42 0zM18 0v30H0V12.614L18 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 							</svg>
 						</div>
-						<h2>
+						<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6172294/">
 							La pratique de l’exercice physique régulier diminue le risque de maladie cardiovasculaire et d’obésité.
-						</h2>
-						<p>Cardiovascular Effects and Benefits of Exercise</p>
+						</a>
+						<div class="logo">
+							<img src="img/home/photo-icn-3.png">
+						</div>
 					</div>
 				</div>
 				<div class="el">
 					<div class="bg" style="background-image: url(img/home/photo-bg-4.jpg);"></div>
+					<div class="overlay"></div>
 					<div class="wrapper">
 						<div class="elAnim__slide">
 							<svg class="icn" viewBox="0 0 42 30" xmlns="http://www.w3.org/2000/svg">
 								<path d="M42 0v30H24V12.614L42 0zM18 0v30H0V12.614L18 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 							</svg>
 						</div>
-						<h2>
+						<a href="https://academic.oup.com/aje/article/187/5/1102/4582884">
 							Les personnes qui pratiquent 1h minimum d’exercices à poids de corps limitent leurs risques de décès toute cause de 29%.
-						</h2>
-						<p>American Journal of Epidemiology.</p>
+						</a>
+						<div class="logo">
+							<img src="img/home/photo-icn-4.png">
+						</div>
 					</div>
 				</div>
 				<div class="container-nav elAnim__fade anim__delayMedium_7">
@@ -364,64 +375,64 @@
 								<div class="info elAnim__slide anim__delayMedium_1">
 									21 kilos perdus en 6 mois
 								</div>
-								<p class="elAnim__slide anim__delayMedium_2">
+								<div class="text elAnim__slide anim__delayMedium_2">
 									<svg viewBox="0 0 42 31" xmlns="http://www.w3.org/2000/svg">
 									  <path d="M42 0v30.383H24.128V12.775L42 0zM17.872 0v30.383H0V12.775L17.872 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 									</svg>
-									Je me sens mieux, aussi bien dans mon corp que dans ma tête
-								</p>
-								<div class="pp elAnim__slide anim__delayMedium_3">
+									<p class="elAnim__slide wrapLine">Je me sens mieux, aussi bien dans mon corp que dans ma tête</p>
+								</div>
+								<div class="pp elAnim__slide anim__delayMedium_6">
 									<div class="bg" style="background-image: url(img/home/quote-pp.jpg);"></div>
 								</div>
-								<div class="name elAnim__slide anim__delayMedium_4">
+								<div class="name elAnim__slide anim__delayMedium_7">
 									Isabelle F.
 								</div>
-								<div class="use elAnim__slide anim__delayMedium_5">
+								<div class="use elAnim__slide anim__delayMedium_8">
 									Utilise fitnext depuis 18 mois
 								</div>
 							</div>
 							<div class="el">
-								<div class="info elAnim__slide anim__delayMedium_1">
-									21 kilos perdus en 6 mois
+								<div class="info">
+									27 kilos perdus en 1 an
 								</div>
-								<p class="elAnim__slide anim__delayMedium_2">
+								<div class="text">
 									<svg viewBox="0 0 42 31" xmlns="http://www.w3.org/2000/svg">
 									  <path d="M42 0v30.383H24.128V12.775L42 0zM17.872 0v30.383H0V12.775L17.872 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 									</svg>
-									Je me sens mieux, aussi bien dans mon corp que dans ma tête
-								</p>
-								<div class="pp elAnim__slide anim__delayMedium_3">
+									<p>J'ai gagné un bien-être intérieur, de l'assurance et je m'apprécie enfin.</p>
+								</div>
+								<div class="pp">
 									<div class="bg" style="background-image: url(img/home/quote-pp.jpg);"></div>
 								</div>
-								<div class="name elAnim__slide anim__delayMedium_4">
+								<div class="name">
 									Isabelle F.
 								</div>
-								<div class="use elAnim__slide anim__delayMedium_5">
+								<div class="use">
 									Utilise fitnext depuis 18 mois
 								</div>
 							</div>
 							<div class="el">
-								<div class="info elAnim__slide anim__delayMedium_1">
-									21 kilos perdus en 6 mois
+								<div class="info">
+									31 kilos perdus en 1 an
 								</div>
-								<p class="elAnim__slide anim__delayMedium_2">
+								<div class="text">
 									<svg viewBox="0 0 42 31" xmlns="http://www.w3.org/2000/svg">
 									  <path d="M42 0v30.383H24.128V12.775L42 0zM17.872 0v30.383H0V12.775L17.872 0z" fill="#E9E9E9" fill-rule="nonzero"/>
 									</svg>
-									Je me sens mieux, aussi bien dans mon corp que dans ma tête
-								</p>
-								<div class="pp elAnim__slide anim__delayMedium_3">
+									<p>J'ai plus d'énergie et mon moral est bien meilleur.</p>
+								</div>
+								<div class="pp">
 									<div class="bg" style="background-image: url(img/home/quote-pp.jpg);"></div>
 								</div>
-								<div class="name elAnim__slide anim__delayMedium_4">
+								<div class="name">
 									Isabelle F.
 								</div>
-								<div class="use elAnim__slide anim__delayMedium_5">
+								<div class="use">
 									Utilise fitnext depuis 18 mois
 								</div>
 							</div>
 						</div>
-						<div class="container-nav elAnim__slide anim__delayMedium_6">
+						<div class="container-nav elAnim__slide anim__delayMedium_9">
 							<div class="arrow left">
 								<svg viewBox="0 0 40 37" xmlns="http://www.w3.org/2000/svg">
 								  <g stroke="#1A2238" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
@@ -545,6 +556,18 @@
 									</svg>
 								</span>
 							</div>
+						</a>
+					</div>
+					<div class="container-btn">
+						<a class="btn" href="">
+							<span class="btn-text">Voir le blog</span>
+							<span class="btn-arrow">
+								<svg viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+								  <g stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+								    <path d="M7.7 1.297l5 5-5 5M12.7 6.3h-11"/>
+								  </g>
+								</svg>
+							</span>
 						</a>
 					</div>
 				</div>
